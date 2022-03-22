d3a4898e500bb54638931d9bffed6ae3348cdc36
DMB29 <mbilaldogan90@gmail.com>
2022-03-23 02:22:37 +0300

    ee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react"
 import './App.css';
 
 function App() {
+
+  const [ad,setAd] =useState(true)
+
+  useEffect(() => {console.log(ad)})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Merhaba, {ad}</p>
+        <button onClick={() => setAd(!ad)}>isim değiştir</button>
+      </div>
+        
     </div>
   );
 }
 
 export default App;
+
+
